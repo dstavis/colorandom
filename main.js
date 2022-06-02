be03c5756397b198bf87dd1373b989d89538149b
Anna Spitz <aspitzemail@gmail.com>
2022-06-01 22:25:16 -0500

    WIP palette class

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-class Color{
-  constructor(){
-    this.hexcode = this.randomHexCode();
+class Color {
+  constructor() {
+    this.hexcode = this.#randomHexCode();
     this.locked = false;
-    this.id = Date.now().toString();
+    this.id = Date.now().toString(); //id no work, fix me
   }
 
-  randomHexCode(){
+  #randomHexCode() {
     var validCharacters = "ABCDEF0123456789".split("");
 
-    var output = ["#"]
+    var output = ["#"];
 
-    for(var i = 0; i < 6; i++){
-      var randomIndex = Math.floor(Math.random() * validCharacters.length )
-      output.push(validCharacters[randomIndex])
+    for(var i = 0; i < 6; i++) {
+      var randomIndex = Math.floor(Math.random() * validCharacters.length);
+      output.push(validCharacters[randomIndex]);
     }
 
-    return output.join('')
+    return output.join('');
   }
+}
+
+class Palette {
+  constructor() {
+    this.colors = this.#addDefaultColors();
+    this.id = Date.now().toString(); //id no work, fix me
+  }
+
+  #addDefaultColors() {
+    var output = [];
+    for(var i = 0; i < 5; i++) {
+      output.push(new Color());
+    }
+    return output;
+  }
+
 }
